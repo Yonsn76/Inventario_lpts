b94881b645a7e9e65414b7078ca2dc18e3d6a45c
Yonsn76 <retobloyons@email.com>
2025-02-23 22:10:55 -0500

    Boton agregado en el incio

--- app/src/main/java/com/san_pedrito/myapplication/InicioFragment.java
+++ app/src/main/java/com/san_pedrito/myapplication/InicioFragment.java
@@ -5,11 +5,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+import android.view.View;
+import android.widget.Button;
+import androidx.fragment.app.FragmentTransaction;
 
 public class InicioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inicio, container, false);
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_inicio, container, false);
+
+        Button btnNuevoRegistro = view.findViewById(R.id.btn_nuevo_registro);
+        btnNuevoRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Navigate to RegistroFragment
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.contenedorFragmentos, new RegistroFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
 }
